b6e08c50aacb6d9dc39b30c8cf41e7b6a8f1ebaf
Helitha Rupasinghe <91548582+hr21don@users.noreply.github.com>
2021-12-12 19:45:12 +0000

    updated comments

--- Bruteforceattack.py
+++ Bruteforceattack.py
@@ -20,7 +20,7 @@ for char in title:
     
 done = False
 def signal_handler(signal, frame):
-    print('You pressed Ctrl+C!')
+    print'You pressed Ctrl+C!'
     global done
     done=True
     sys.exit(0)
@@ -36,14 +36,14 @@ def animate():
 
 
 def _attack(chrs, inputt):
-    print("[+] Start Time: ", time.strftime('%H:%M:%S'))
+    print"[+] Start Time: ", time.strftime('%H:%M:%S')
     start_time = time.time()
     t = threading.Thread(target=animate)
     t.start()
     total_pass_try=0
     for n in range(1, 31+1):
       characterstart_time = time.time()
-      print("\n[!] I'm still at ", n , "-character")
+      print"\n[!] I'm still at ", n , "-character"
       
       for xs in itertools.product(chrs, repeat=n):
           saved = ''.join(xs)
@@ -56,19 +56,19 @@ def _attack(chrs, inputt):
               global done
               done = True
 
-              print("\n[!] found ", stringg)
-              print("\n[-] End Time: ", time.strftime('%H:%M:%S'))
-              print("\n[-] Total Keyword attempted: ", total_pass_try)
-              print("\n---Md5 cracked at %s seconds ---" % (time.time() - start_time))
+              print"\n[!] found ", stringg
+              print"\n[-] End Time: ", time.strftime('%H:%M:%S')
+              print"\n[-] Total Keyword attempted: ", total_pass_try
+              print"\n---Md5 cracked at %s seconds ---" % (time.time() - start_time)
               sys.exit("Thank You !")
         
-      print("\n[!]",n,"-character finished in %s seconds ---" % (time.time() - characterstart_time))
+      print"\n[!]",n,"-character finished in %s seconds ---" % (time.time() - characterstart_time)
 
 def main():
-    print ('''
+    print '''
 #============================#
 # Welcome To Bruteforce Meta #
-# ===========================#\n''')
+# ===========================#\n'''
     
     inp_usr = input(" add md5\n")
     chrs = string.printable.replace(' \t\n\r\x0b\x0c', '')
